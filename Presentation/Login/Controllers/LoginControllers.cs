ba4ec4824880e592aac9ca553f396ea9d2af360f
Alexis Sanchez <alexis.ingesis@gmail.com>
2025-08-25 22:32:49 -0500

    Agregar archivos de proyecto.

---
+++ Presentation/Login/Controllers/LoginControllers.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    [Route("[controller]/")]
+    [ApiController]
+    public class LoginControllers : ControllerBase
+    {
+        [HttpPost("create-user")]
+        [Route("create-user")]
+        public async Task<IActionResult> CreateUser()
+        {
+            return Created();
+        }
+
+        [HttpPost]
+        [Route("delete-user")]
+        public async Task<IActionResult> DeleteUser()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("update-user")]
+        public async Task<IActionResult> UpdateUser()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("get-user")]
+        public async Task<IActionResult> GetUser()
+        {
+            return StatusCode(StatusCodes.Status200OK, new List<int> () );
+        }
+    }
+}
